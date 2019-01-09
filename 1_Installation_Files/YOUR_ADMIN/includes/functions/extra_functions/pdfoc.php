9acbe84824baec7bd2ff8cb34c63f6abc26554db
Zen4All <info@zen4all.nl>
2019-01-09 16:50:51 +0100

    restore functions
    I was a little to rigorous in removing "unused" functions

--- 1_Installation_Files/YOUR_ADMIN/includes/functions/extra_functions/pdfoc.php
+++ 1_Installation_Files/YOUR_ADMIN/includes/functions/extra_functions/pdfoc.php
@@ -90,3 +90,141 @@ function pdfoc_verify_date($date)
 
   return $error;
 }
+
+/**
+ * 
+ * @global array $db
+ * @param int $order_status_id
+ * @param int $key
+ * @return string
+ */
+function zen_cfg_pull_down_order_statuses_plus_none($order_status_id, $key = '')
+{
+  global $db;
+  $name = (($key) ? 'configuration[' . $key . ']' : 'configuration_value');
+
+  $statuses_array = [[
+  'id' => '0',
+  'text' => PDFOC_TEXT_NONE]];  // This is defined in admin/includes/languages/english/extra_definitions/pdfoc.php
+  $statuses = $db->Execute("SELECT orders_status_id, orders_status_name
+                            FROM " . TABLE_ORDERS_STATUS . "
+                            WHERE language_id = " . (int)$_SESSION['languages_id'] . "
+                            ORDER BY orders_status_name");
+
+  foreach ($statuses as $status) {
+    $statuses_array[] = [
+      'id' => $status['orders_status_id'],
+      'text' => $status['orders_status_name'] . ' [' . $status['orders_status_id'] . ']'];
+  }
+
+  return zen_draw_pull_down_menu($name, $statuses_array, $order_status_id);
+}
+
+/**
+ * 
+ * @global array $db
+ * @param int $order_status_id
+ * @param int $language_id
+ * @return string
+ */
+function zen_get_order_status_name_plus_none($order_status_id, $language_id = '')
+{
+  global $db;
+
+  if ($order_status_id < 1) {
+    return PDFOC_TEXT_NONE;  // This is defined in admin/includes/languages/english/extra_definitions/pdfoc.php
+  }
+  if (!is_numeric($language_id)) {
+    $language_id = $_SESSION['languages_id'];
+  }
+
+  $status = $db->Execute("SELECT orders_status_name
+                          FROM " . TABLE_ORDERS_STATUS . "
+                          WHERE orders_status_id = " . (int)$order_status_id . "
+                          AND language_id = " . (int)$language_id);
+
+  return $status->fields['orders_status_name'] . ' [' . (int)$order_status_id . ']';
+}
+
+/**
+ * 
+ * @param int $template_id
+ * @param int $key
+ * @return string
+ */
+function pdfoc_cfg_pull_down_templates($template_id, $key = '')
+{
+
+  $name = (($key) ? 'configuration[' . $key . ']' : 'configuration_value');
+
+  $directory = DIR_PDFOC_TEMPLATES;
+  $resc = opendir($directory);
+
+  $file_type_array = [[
+  'id' => '0',
+  'text' => PDFOC_TEXT_NONE]];  // This is defined in admin/includes/languages/english/extra_definitions/pdfoc.php
+  while ($file = readdir($resc)) {
+
+    $ext = strrchr($file, ".");
+
+    if ($ext == ".php") {
+
+//      $filename = str_replace('_', " ",$file);
+      $filename = str_replace('-', '_', $file);
+      $filename = str_replace($ext, "", $filename);
+      $fileconst = 'PDFOC_TEMPLATE_NAME_' . strtoupper($filename);
+      /* look for a constant for that filename; if exists, use it, otherwise use filename */
+      /* (allows language-specific names to be displayed in the dropdown menus) */
+      if (defined("$fileconst")) {
+        $filename = constant($fileconst);
+      } else {
+        $filename = "MISTAKE! " . $filename;    // debugging code
+      }
+      $file_type_array[] = array('id' => $file, 'text' => $filename);
+    } // EOIF $ext
+  }  // EOWHILE $file
+
+  return zen_draw_pull_down_menu($name, $file_type_array, $template_id);
+}
+
+/**
+ * 
+ * @param int $template_id
+ * @return string
+ */
+function pdfoc_get_template_name($template_id)
+{
+
+  $directory = DIR_PDFOC_TEMPLATES;
+  $resc = opendir($directory);
+
+  if ($template_id == '0') {
+    return PDFOC_TEXT_NONE;  // This is defined in admin/includes/languages/english/extra_definitions/pdfoc.php
+  }
+
+  while ($file = readdir($resc)) {
+
+    $ext = strrchr($file, ".");
+
+    if ($ext == ".php") {
+
+//      $filename = str_replace('_', " ",$file);
+      $filename = str_replace('-', '_', $file);
+      $filename = str_replace($ext, "", $filename);
+      $fileconst = 'PDFOC_TEMPLATE_NAME_' . strtoupper($filename);
+      /* look for a constant for that filename; if exists, use it, otherwise use filename */
+      /* (allows language-specific names to be displayed in the dropdown menus) */
+      if (defined("$fileconst")) {
+        $filename = constant($fileconst);
+      } else {
+        $filename = "MISTAKE! " . $filename;    // debugging code
+      }
+
+      if ($file == $template_id) {
+        break;
+      }
+    } // EOIF $ext
+  }  // EOWHILE $file
+
+  return $filename;
+}
