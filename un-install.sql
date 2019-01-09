c980dd2556ef3b4aef751ba166dcc109934183b7
Zen4All <info@zen4all.nl>
2019-01-09 14:55:22 +0100

    update sql files

--- un-install.sql
+++ un-install.sql
@@ -1,14 +1,10 @@
-#NEXT_X_ROWS_AS_ONE_COMMAND:3
 SELECT @pdfocid:=configuration_group_id
 FROM configuration_group
 WHERE configuration_group_title='PDF Order Center';
 
-#NEXT_X_ROWS_AS_ONE_COMMAND:2
 DELETE FROM configuration
 WHERE configuration_group_id=@pdfocid;
 
-#NEXT_X_ROWS_AS_ONE_COMMAND:3
 DELETE FROM configuration_group
 WHERE configuration_group_id=@pdfocid
 LIMIT 1;
-
